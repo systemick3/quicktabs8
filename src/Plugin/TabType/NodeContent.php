6ca945cc93296a04938f18a820ec372c45edbacf
Mike Garthwaite <mike.garthwaite@ecorys.com>
2016-08-04 11:57:04 +0100

    Fixed default value bug

--- src/Plugin/TabType/NodeContent.php
+++ src/Plugin/TabType/NodeContent.php
@@ -22,30 +22,31 @@ class NodeContent extends TabTypeBase {
    * {@inheritdoc}
    */
   public function optionsForm($tab) {
+    $plugin_name =  $this->getPluginDefinition()['name'];
     $form = array();
-    $form['node']['nid'] = array(
+    $form['nid'] = array(
       '#type' => 'textfield',
       '#title' => t('Node'),
       '#description' => t('The node ID of the node.'),
       '#maxlength' => 10,
       '#size' => 20,
-      '#default_value' => isset($tab['nid']) ? $tab['nid'] : '',
+      '#default_value' => isset($tab['content'][$plugin_name]['options']['nid']) ? $tab['content'][$plugin_name]['options']['nid'] : '',
     );
     $view_modes = \Drupal::entityManager()->getViewModes('node');
     $options = array();
     foreach ($view_modes as $view_mode_name => $view_mode) {
       $options[$view_mode_name] = $view_mode['label'];
     }
-    $form['node']['view_mode'] = array(
+    $form['view_mode'] = array(
       '#type' => 'select',
       '#title' => t('View mode'),
       '#options' => $options,
-      '#default_value' => isset($tab['view_mode']) ? $tab['view_mode'] : 'full',
+      '#default_value' => isset($tab['content'][$plugin_name]['options']['view_mode']) ? $tab['content'][$plugin_name]['options']['view_mode'] : 'full',
      );
-     $form['node']['hide_title'] = array(
+     $form['hide_title'] = array(
       '#type' => 'checkbox',
       '#title' => t('Hide the title of this node'),
-      '#default_value' => isset($tab['hide_title']) ? $tab['hide_title'] : 1,
+      '#default_value' => isset($tab['content'][$plugin_name]['options']['hide_title']) ? $tab['content'][$plugin_name]['options']['hide_title'] : 1,
     );
     return $form;
   }
