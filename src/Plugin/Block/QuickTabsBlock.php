f0172ed7f781638d3752da741d8e1ccbc8441eac
Mike Garthwaite <mike.garthwaite@ecorys.com>
2016-08-20 14:17:31 +0100

    Rendering blocks in a QuickTabs block

--- src/Plugin/Block/QuickTabsBlock.php
+++ src/Plugin/Block/QuickTabsBlock.php
@@ -8,6 +8,8 @@
 namespace Drupal\quicktabs\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+Use Drupal\Core\Url;
+Use Drupal\Core\Link;
 
 /**
  * Provides a 'QuickTabs' block.
@@ -27,15 +29,39 @@ class QuickTabsBlock extends BlockBase {
    */
   public function build() {
     $build = array();
+    $titles = array();
     $block_id = $this->getDerivativeId();
     
     $type = \Drupal::service('plugin.manager.tab_type');
     $plugin_definitions = $type->getDefinitions();
     $qt = \Drupal::service('entity.manager')->getStorage('quicktabs_instance')->load($block_id);
+    $current_path = \Drupal::service('path.current')->getPath();
+    $tab_number = 0;
     foreach ($qt->getConfigurationData() as $index => $tab) {
+      $options = array(
+        'query' => array('qt-quicktabs' => $tab_number),
+        'fragment' => 'qt-quicktabs',
+        'attributes' => array('id' => 'quicktabs-tab-quicktabs-' . $tab_number),
+      );
+      $titles[] = Link::fromTextAndUrl($tab['title'], Url::fromUri('internal:' . $current_path, $options));
       $object = $type->createInstance($tab['type']);
       $build[$index] = $object->render($tab['content'][$tab['type']]['options']);
+      $tab_number++;
     }
+
+    $tabs = array(
+      '#theme' => 'item_list',
+      '#items' => $titles,
+      '#attributes' => array(
+        'class' => array('quicktabs-tabs'), 
+      ),
+    );
+    
+    array_unshift($build, $tabs);
+  
+    $build['#attached'] = array(
+      'library' => array('quicktabs/quicktabs'),
+    );
     
     return $build;
   }
