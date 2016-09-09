f9f80dcb1764ac82017ce22a59a3fb276c03715b
Ecorys UK <mike.garthwaite@ecorys.com>
2016-09-09 11:31:41 +0100

    Refactored code to produce render array

--- src/Entity/QuickTabsInstance.php
+++ src/Entity/QuickTabsInstance.php
@@ -8,6 +8,9 @@ namespace Drupal\quicktabs\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityBase;
 
+Use Drupal\Core\Link;
+Use Drupal\Core\Url;
+Use Drupal\Core\Template\Attribute;
 
 /**
  * Defines the QuickTabsInstance entity.
@@ -163,6 +166,146 @@ class QuickTabsInstance extends ConfigEntityBase implements QuickTabsInstanceInt
    */
   public function setConfigurationData($configuration_data) {
     $this->configuration_data = $configuration_data;
+  }
+
+  /**
+   * Returns a render array to be used in a block or page.
+   *
+   * @return array a render array
+   */
+  public function getRenderArray() {
+    $qt_id = $this->id();
+    $type = \Drupal::service('plugin.manager.tab_type');
+
+    // The render array used to build the block
+    $build = array();
+    $build['pages'] = array();
+    $build['pages']['#theme_wrappers'] = array(
+      'container' => array(
+        '#attributes' => array(
+          'class' => array('quicktabs-main'),
+          'id' => 'quicktabs-container-' . $qt_id,
+        ),
+      ),
+    );
+
+    // Pages of content that will be shown or hidden
+    $tab_pages = array();
+
+    // Tabs used to show/hide content
+    $titles = array();
+
+    foreach ($this->getConfigurationData() as $index => $tab) {
+      // Build the pages //////////////////////////////////////
+      if ($this->isAjax()) {
+        if ($this->getDefaultTab() == $index) {
+          $object = $type->createInstance($tab['type']);
+          $render = $object->render($tab);
+        }
+        else {
+          $render = array('#markup' => 'Content goes here');
+        }
+      }
+      else {
+        $object = $type->createInstance($tab['type']);
+        $render = $object->render($tab);
+      }
+
+      $classes = array('quicktabs-tabpage');
+
+      if ($this->getDefaultTab() != $index) {
+        $classes[] = 'quicktabs-hide';
+      }
+
+      $attributes = new Attribute(array('id' => 'quicktabs-tabpage-' . $qt_id . '-' . $index));
+      $attributes['class'] = $classes;
+      $render['#prefix'] = '<div ' . $attributes . '>';
+      $render['#suffix'] = '</div>';
+
+      $build['pages'][$index] = $render;
+
+      // Build the tabs ///////////////////////////////
+      $options = array(
+        'query' => array('qt-quicktabs' => $index),
+        'fragment' => 'qt-quicktabs',
+        'attributes' => array('id' => 'quicktabs-tab-' . $qt_id . '-' . $index),
+      );
+      $wrapper_attributes = array();
+      if ($this->getDefaultTab() == $index) {
+        $wrapper_attributes['class'] = array('active');
+      }
+
+      $link_classes = array();
+      if ($this->isAjax()) {
+        $link_classes[] = 'use-ajax';
+
+        if ($this->getDefaultTab() == $index) {
+          $link_classes[] = 'quicktabs-loaded';
+        }
+      }
+      else {
+        $link_classes[] = 'quicktabs-loaded';
+      }
+
+      $titles[] = array(
+        '0' => Link::fromTextAndUrl(
+          $tab['title'],
+          Url::fromRoute(
+            'quicktabs.ajax_content',
+            array(
+              'js' => 'nojs',
+              'instance' => $qt_id,
+              'tab' => $index
+            ),
+            array(
+              'attributes' => array(
+                'class' => $link_classes,
+              ),
+            )
+          )
+        )->toRenderable(),
+        '#wrapper_attributes' => $wrapper_attributes,
+      );
+
+      // Array of tab pages to pass as settings ////////////
+      $tab['tab_page'] = $index;
+      $tab_pages[] = $tab;
+    }
+
+    $tabs = array(
+      '#theme' => 'item_list',
+      '#items' => $titles,
+      '#attributes' => array(
+        'class' => array('quicktabs-tabs'),
+      ),
+    );
+
+    // Add tabs to the build
+    array_unshift($build, $tabs);
+
+    // Attach js
+    $build['#attached'] = array(
+      'library' => array('quicktabs/quicktabs'),
+      'drupalSettings' => array(
+        'quicktabs' => array(
+          'qt_' . $qt_id => array(
+            'tabs' => $tab_pages,
+          ),
+        ),
+      ),
+    );
+
+    // Add a wrapper
+    $build['#theme_wrappers'] = array(
+      'container' => array(
+        '#attributes' => array(
+          'class' => array('quicktabs-wrapper'),
+          'id' => 'quicktabs-' . $qt_id,
+        ),
+      ),
+    );
+
+    return $build;
   }
 
   /**
