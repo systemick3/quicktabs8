25c2cf6e559c276836fdd9627a6b9fb89eddb389
Ecorys UK <mike.garthwaite@ecorys.com>
2016-12-16 18:58:33 +0000

    Test form qt creation and deletion

--- src/Tests/QuickTabsAdminTest.php
+++ src/Tests/QuickTabsAdminTest.php
@@ -11,6 +11,7 @@ use Drupal\simpletest\WebTestBase;
 use Drupal\taxonomy\Entity\Vocabulary;
 use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\quicktabs\Entity\QuickTabsInstance;
 
 /**
  * Tests creating and saving a QuickTabs instance..
@@ -26,12 +27,7 @@ class QuickTabsAdminTest extends WebTestBase {
    */
   protected $adminUser;
 
-  /**
-   * A n array of vocabularies.
-   *
-   * @var \Drupal\user\UserInterface
-   */
-  //protected $vocabularies;
+  protected $strictConfigSchema = FALSE;
   
   /**
    * Modules to enable.
@@ -69,43 +65,83 @@ class QuickTabsAdminTest extends WebTestBase {
 
     $this->drupalLogin($this->adminUser);
 
-    //$this->vocabularies = array();
-    
-    /*$vocabulary1 = entity_create('taxonomy_vocabulary', array(
-      'name' => $this->randomMachineName(),
-      'description' => $this->randomMachineName(),
-      'vid' => Unicode::strtolower($this->randomMachineName()),
-      'langcode' => LanguageInterface::LANGCODE_NOT_SPECIFIED,
-      'weight' => mt_rand(0, 10),
+    // Create an article content type
+    $this->drupalCreateContentType(array(
+      'type' => 'article',
     ));
-    $vocabulary1->save();
-    $this->vocabularies[] = $vocabulary1;
-    
-    $vocabulary2 = entity_create('taxonomy_vocabulary', array(
-      'name' => $this->randomMachineName(),
-      'description' => $this->randomMachineName(),
-      'vid' => Unicode::strtolower($this->randomMachineName()),
-      'langcode' => LanguageInterface::LANGCODE_NOT_SPECIFIED,
-      'weight' => mt_rand(0, 10),
-    ));
-    $vocabulary2->save();
-    $this->vocabularies[] = $vocabulary2;*/
   }
 
   /**
    * Test all vocabularies appear on admin page.
    */
-  function testAllVocabulariesLoaded() {
+  function testQuicktabsAdmin() {
     $this->drupalGet('admin/structure/quicktabs');
     $this->assertResponse(200);
     $this->assertRaw('Quick Tabs');
     $this->drupalGet('admin/structure/quicktabs/add');
     $this->assertResponse(200);
     $this->assertRaw('Add Quicktabs Instance');
-    //$this->assertRaw('Each Quicktabs instance has a corresponding block');
+    $this->assertRaw('Name');
+    $this->assertRaw('Renderer');
+    $this->assertRaw('Default tab');
+    $this->assertRaw('Hide empty tabs');
+    $this->assertRaw('Add tab');
+    $this->assertRaw('Save');
 
-    //foreach($this->vocabularies as $vocabulary) {
-      //$this->assertRaw($vocabulary->get('name'));
-    //}
+    $node1 = $this->drupalCreateNode(array(
+      'title' => t('Node 1'),
+      'type' => 'article',
+    ));
+    $node2 = $this->drupalCreateNode(array(
+      'title' => t('Node 2'),
+      'type' => 'article',
+    ));
+
+    $edit = [
+      'label' => $this->randomMachineName(),
+      'id' => strtolower($this->randomMachineName()),
+      'renderer' => 'quick_tabs',
+      'options[quick_tabs][ajax]' => 0,
+      'hide_empty_tabs' => 1,
+      'default_tab' => 9999,
+      'configuration_data[0][title]' => $this->randomMachineName(),
+      'configuration_data[0][type]' => 'node_content',
+      'configuration_data[0][content][node_content][options][nid]' => $node1->id(),
+      'configuration_data[0][content][node_content][options][view_mode]' => 'full',
+      'configuration_data[0][content][node_content][options][hide_title]' => 1,
+      'configuration_data[1][title]' => $this->randomMachineName(),
+      'configuration_data[1][type]' => 'node_content',
+      'configuration_data[1][content][node_content][options][nid]' => $node2->id(),
+      'configuration_data[1][content][node_content][options][view_mode]' => 'full',
+      'configuration_data[1][content][node_content][options][hide_title]' => 1,
+    ];
+
+    $this->drupalPostForm('admin/structure/quicktabs/add', $edit, t('Save'));
+
+    $qt = \Drupal::service('entity.manager')->getStorage('quicktabs_instance')->load($edit['id']);
+
+    $this->assertEqual('Drupal\quicktabs\Entity\QuickTabsInstance', get_class($qt));
+    $this->assertEqual($qt->id(), $edit['id']);
+    $this->assertEqual($qt->label(), $edit['label']);
+    $this->assertEqual($qt->getRenderer(), $edit['renderer']);
+    $this->assertEqual($qt->getHideEmptyTabs(), $edit['hide_empty_tabs']);
+    $this->assertEqual($qt->getDefaultTab(), $edit['default_tab']);
+
+    $configurationData = $qt->getConfigurationData();
+    $this->assertEqual($configurationData[0]['title'], $edit['configuration_data[0][title]']);
+    $this->assertEqual($configurationData[1]['title'], $edit['configuration_data[1][title]']);
+    $this->assertEqual($configurationData[0]['type'], $edit['configuration_data[0][type]']);
+    $this->assertEqual($configurationData[1]['type'], $edit['configuration_data[1][type]']);
+    $this->assertEqual($configurationData[0]['content']['node_content']['options']['nid'], $edit['configuration_data[0][content][node_content][options][nid]']);
+    $this->assertEqual($configurationData[1]['content']['node_content']['options']['nid'], $edit['configuration_data[1][content][node_content][options][nid]']);
+    $this->assertEqual($configurationData[0]['content']['node_content']['options']['view_mode'], $edit['configuration_data[0][content][node_content][options][view_mode]']);
+    $this->assertEqual($configurationData[1]['content']['node_content']['options']['view_mode'], $edit['configuration_data[1][content][node_content][options][view_mode]']);
+    $this->assertEqual($configurationData[0]['content']['node_content']['options']['hide_title'], $edit['configuration_data[0][content][node_content][options][hide_title]']);
+    $this->assertEqual($configurationData[1]['content']['node_content']['options']['hide_title'], $edit['configuration_data[1][content][node_content][options][hide_title]']);
+
+    $this->drupalPostForm('admin/structure/quicktabs/' . $qt->id() . '/delete', [], t('Delete'));
+
+    $qt = \Drupal::service('entity.manager')->getStorage('quicktabs_instance')->load($edit['id']);
+    $this->assertNull($qt, t('Quicktabs instance not found in database'));
   }
 }
