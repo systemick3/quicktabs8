11fb6674ef66fc661d1324e41d229f82dd028c16
systemick <michaelgarthwaite@gmail.com>
2017-04-30 20:00:13 +0100

    First draft of code to get grouping field working

--- src/Plugin/views/style/Quicktabs.php
+++ src/Plugin/views/style/Quicktabs.php
@@ -4,6 +4,9 @@ namespace Drupal\quicktabs\Plugin\views\style;
 
 use Drupal\core\form\FormStateInterface;
 use Drupal\views\Plugin\views\style\StylePluginBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Style plugin to render views rows as tabs.
@@ -33,7 +36,7 @@ class Quicktabs extends StylePluginBase {
    *
    * @var bool
    */
-  protected $usesRowClass = TRUE;
+  //protected $usesRowClass = TRUE;
 
   /**
    * Should field labels be enabled by default.
@@ -41,6 +44,13 @@ class Quicktabs extends StylePluginBase {
    * @var bool
    */
   protected $defaultFieldLabels = TRUE;
+
+  /**
+   * Should field labels be enabled by default.
+   *
+   * @var bool
+   */
+  protected $setMapping;
   
   /**
    * Set default options
@@ -49,6 +59,20 @@ class Quicktabs extends StylePluginBase {
     $options = parent::defineOptions();
     $options['path'] = array('default' => 'quicktabs');
     return $options;
+  }
+
+  /**
+   * Set the set mapping.
+   */
+  public function setSetMapping(array $setMapping) {
+    $this->setMapping = $setMapping;
+  }
+
+  /**
+   * Get the set mapping.
+   */
+  public function getSetMapping() {
+    return $this->setMapping;
   }
 
   /**
@@ -78,5 +102,141 @@ class Quicktabs extends StylePluginBase {
       '#description' => t('Select the field that will be used as the tab title.'),
       '#weight' => -3,
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    if ($this->usesRowPlugin() && empty($this->view->rowPlugin)) {
+      debug('Drupal\views\Plugin\views\style\StylePluginBase: Missing row plugin');
+      return;
+    }
+
+    // Group the rows according to the grouping instructions, if specified.
+    $sets = $this->renderGrouping(
+      $this->view->result,
+      $this->options['grouping'],
+      TRUE
+    );
+
+    return $this->renderGroupingSets($sets);
+  }
+
+  public function renderGroupingSets($sets, $level = 0) {
+    $output = array();
+    $theme_functions = $this->view->buildThemeFunctions($this->groupingTheme);
+    $tab_titles = [];
+    $link_classes = ['loaded'];
+    $quicktab_id = str_replace('_', '-', $this->view->id());
+    foreach ($sets as $index => $set) {
+      $set_index = $index;
+      if (!empty($this->options['grouping'])) {
+        $this->view->set_count = count($sets);
+        $tab_titles[] = [
+          '0' => Link::fromTextAndUrl(
+            new TranslatableMarkup($index),
+            Url::fromRoute(
+              'quicktabs.ajax_content',
+              [
+                'js' => 'nojs',
+                'instance' => $quicktab_id,
+                'tab' => $index
+              ],
+              [
+                'attributes' => array(
+                  'class' => $link_classes,
+                ),
+              ]
+            )
+          )->toRenderable(),
+        ];
+      }
+      else {
+        $tab_title_field = $this->options['tab_title_field'];
+        foreach($set['rows'] as $row) {
+          $entity = $row->_entity;
+          $tab_titles[] = [
+            '0' => Link::fromTextAndUrl(
+              new TranslatableMarkup($entity->get($tab_title_field)->value),
+              Url::fromRoute(
+                'entity.node.canonical',
+                [
+                  'node' => $entity->id(),
+                ],
+                [
+                  'attributes' => [
+                    'class' => $link_classes,
+                  ],
+                ]
+              )
+            )->toRenderable(),
+          ];
+        }
+      }
+
+      $level = isset($set['level']) ? $set['level'] : 0;
+
+      $row = reset($set['rows']);
+      // Render as a grouping set.
+      if (is_array($row) && isset($row['group'])) {
+        $single_output = array(
+          '#theme' => $theme_functions,
+          '#view' => $this->view,
+          '#grouping' => $this->options['grouping'][$level],
+          '#rows' => $set['rows'],
+        );
+      }
+      // Render as a record set.
+      else {
+        if ($this->usesRowPlugin()) {
+          foreach ($set['rows'] as $index => $row) {
+            $this->view->row_index = $index;
+            $set['rows'][$index] = $this->view->rowPlugin->render($row);
+          }
+        }
+
+        $single_output = $this->renderRowGroup($set['rows']);
+      }
+
+      $single_output['#grouping_level'] = $level;
+      $single_output['#title'] = $set['group'];
+
+      if (!empty($this->options['grouping'])) {
+        $set_mapping = [];
+        foreach($sets as $set_index => $set) {
+          foreach ($set['rows'] as $row_index => $row) {
+            $set_mapping[$set_index][] = $row_index;
+          }
+        }
+        $this->setSetMapping($set_mapping);
+      }
+
+      $output[] = $single_output;
+    }
+    unset($this->view->row_index);
+
+    $tabs = [
+      '#theme' => 'item_list',
+      '#items' => $tab_titles,
+      '#attributes' => [
+        'class' => ['quicktabs-tabs'],
+      ],
+    ];
+
+    // Add tabs to the build
+    array_unshift($output, $tabs);
+
+    // Add quicktabs wrapper to all the output
+    $output['#theme_wrappers'] = [
+      'container' => [
+        '#attributes' => [
+          'class' => ['quicktabs-wrapper'],
+          'id' => 'quicktabs-' . $quicktab_id,
+        ],
+      ],
+    ];
+
+    return $output;
   }
 }
